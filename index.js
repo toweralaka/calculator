2d6fd5c0c8ed04b7225730267458ebc73df1e6f0
ola <olaidealaka12@gmail.com>
2025-06-04 15:18:43 +0100

    feat: Add functions for some buttons
    Add event listeners and functions to handle
    decimal, delete, clear and operators' button
    click

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ let firstNumber = "";
 let secondNumber = "";
 let currentNumber = "";
 let operator = "";
-let operatorOn = false;
+let operatorOn = true;
+let decimalUsed = false;
+let clearScreen = true;
+let canDelete = false;
 let screen = document.querySelector(".screen");
+let decimalBtn = document.querySelector(".decimal");
+let equateBtn = document.querySelector(".equate");
+let deleteBtn = document.querySelector(".delete");
+let clearBtn = document.querySelector(".clear");
+
 const add = () => {
     console.log("add")
 }
@@ -22,10 +30,84 @@ const divide = () => {
 
 const operate = () => {
     console.log("operate")
+    console.log(screen.textContent)
 }
 
-document.querySelectorAll(".button").forEach(element=>{
+const resetVariables = () => {
+    operatorOn = true;
+    decimalUsed = false;
+    clearScreen = true;
+}
+const disableDecimal = () => {
+    decimalBtn.disabled = true
+}
+
+document.querySelectorAll(".number").forEach(element=>{
     element.addEventListener("click", event =>{
-        screen.textContent = screen.textContent + event.target.textContent
+        if(clearScreen == true){
+            screen.textContent = event.target.textContent;
+            clearScreen = false;
+            canDelete = true;
+        }else{
+            screen.textContent = screen.textContent + event.target.textContent;
+        }
+        operatorOn = false;
     })
+})
+
+document.querySelectorAll(".operator").forEach(element=>{
+    element.addEventListener("click", event =>{
+        if(operatorOn == false){
+            operatorOn = true;
+            screen.textContent = screen.textContent + event.target.textContent
+        }
+    })
+})
+
+decimalBtn.addEventListener("click", (event)=>{
+    if(decimalUsed == false){
+        decimalUsed = true;
+        clearScreen = false;
+        canDelete = true
+        screen.textContent = screen.textContent + event.target.textContent
+    }
+})
+
+equateBtn.addEventListener("click", ()=>{
+    if(operatorOn == false){
+        operate()
+        resetVariables()
+    }
+})
+
+deleteBtn.addEventListener("click", ()=>{
+    if(canDelete == true){
+        let oldVal = screen.textContent;
+        let valueLength = oldVal.length;
+        let oldValStr = oldVal.split("");
+        let operatorArray = ["+", "-", "x", "÷"];
+        let lastIndex = valueLength - 1;
+        //check if operator is deleted
+        if(operatorArray.includes(oldValStr[lastIndex])){
+            operatorOn = false;
+        }
+        //check if decimal is deleted
+        if(oldValStr[-1] == "."){
+            decimalUsed = false;
+        }
+        let newValue = oldValStr.slice(0, valueLength-1)
+            .join("");
+        if(newValue.length < 1){
+            canDelete = false;
+            clearScreen = true;
+            screen.textContent = 0;
+        }else{
+            screen.textContent = newValue;
+        }
+    }
+})
+
+clearBtn.addEventListener("click", ()=>{
+    resetVariables()
+    screen.textContent = "";
 })
